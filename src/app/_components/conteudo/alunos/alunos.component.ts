0b6b4d0a9788bb27c7ef89c947bff87fa4583d37
Cristiano Fagundes <fagundescristianof@gmail.com>
2018-11-30 15:59:53 -0200

    Toast e pop up

--- src/app/_components/conteudo/alunos/alunos.component.ts
+++ src/app/_components/conteudo/alunos/alunos.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { PaginationInstance } from 'ngx-pagination';
+import { Router } from '@angular/router';
 import { AlunoNota } from '@app/_models/alunoNota';
 import { Aluno } from '@app/_models/aluno';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-alunos',
@@ -9,28 +15,63 @@ import { Aluno } from '@app/_models/aluno';
 })
 export class AlunosComponent implements OnInit {
 
-  constructor() { }
+  public filter = '';
+  public maxSize = 7;
+  public directionLinks = true;
+  public autoHide = false;
+  public responsive = false;
+  public config: PaginationInstance = {
+    id: 'advanced',
+    itemsPerPage: 10,
+    currentPage: 1
+  };
+  public labels: any = {
+    previousLabel: 'Anterior',
+    nextLabel: 'Próxima',
+    screenReaderPaginationLabel: 'Paginação',
+    screenReaderPageLabel: 'página',
+    screenReaderCurrentLabel: 'Você está na página'
+  };
 
-  disciplina : AlunoNota[] = []
-  aluno : Aluno
+
+  constructor(private router: Router, private modalService: BsModalService, private toastr: ToastrService) { }
+
+  modalRef: BsModalRef;
+  public alunos: Aluno[] = [];
+  delete :any
 
   ngOnInit() {
-    this.aluno = new Aluno();
-    this.aluno.nome = "Peteca"
-    this.aluno.curso = "Programador de Peteca"
-    this.aluno.email = "peteca@peteca.com"
-    this.aluno.telefone = "40028922"
+    for(let i=0; i<100; i++){
+      let c = new Aluno();
+      c.nome = 'Alunos '  + i;
+      this.alunos.push(c);
+    }
+  }
 
-    this.disciplina.push(new AlunoNota(10,0,"IA"));
-    this.disciplina.push(new AlunoNota(10,0,"IA"));
-    this.disciplina.push(new AlunoNota(10,0,"IA"));
-    this.disciplina.push(new AlunoNota(10,0,"IA"));
-    this.disciplina.push(new AlunoNota(10,0,"IA"));
-    this.disciplina.push(new AlunoNota(10,0,"IA"));
-    this.disciplina.push(new AlunoNota(10,0,"IA"));
+  onPageChange(number: number) {
+    this.config.currentPage = number;
+  }
 
+  openModal(template: TemplateRef<any>, dele) {
+    this.delete = dele;
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
 
+  confirm(): void {
 
+    var numero = this.delete.nome;
+    var index = this.alunos.map(x => {
+      return x.nome;
+    }).indexOf(numero);
+
+    this.alunos.splice(index, 1);
+    this.modalRef.hide();
+    this.toastr.success("Removido com sucesso");
+  }
+
+  decline(): void {
+
+    this.modalRef.hide();
   }
 
 }
