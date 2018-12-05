eba8c387d1351752e25ce8c7692dadb6d4a4a992
Cristiano Fagundes <fagundescristianof@gmail.com>
2018-12-05 19:22:21 -0200

    aaa

--- node/app.js
+++ node/app.js
@@ -9,13 +9,16 @@ var fs = require('fs');
 
 
 
-https.createServer({
-  key: fs.readFileSync('privatekey.pem'),
-  cert: fs.readFileSync('certificate.pem')
-}, app).listen(3000, ()=> console.log("Rodando express com HTTPS") );
+// https.createServer({
+//   key: fs.readFileSync('privatekey.pem'),
+//   cert: fs.readFileSync('certificate.pem')
+// }, app).listen(3000, ()=> console.log("Rodando express com HTTPS") );
 
 
-
+https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('hello world\n');
+}).listen(3000);
 
 // app.listen(3000, function(){
 
