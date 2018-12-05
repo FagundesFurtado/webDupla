c0a68829c5ef710c7edc7a0b540302bab57af3c6
Cristiano Fagundes <fagundescristianof@gmail.com>
2018-12-05 21:22:37 -0200

    Corsss

--- node/config/server.js
+++ node/config/server.js
@@ -28,8 +28,10 @@ app.use(expressSession({
 }))
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 // app.use(cors());
