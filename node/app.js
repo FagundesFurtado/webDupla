ec6d964d9d5383bd01fc0cd975c3a631986c19c2
Cristiano Fagundes <fagundescristianof@gmail.com>
2018-12-05 19:16:11 -0200

    sei la

--- node/app.js
+++ node/app.js
@@ -7,15 +7,12 @@ var https = require('https');
 var http = require('http');
 var fs = require('fs');
 
-// This line is from the Node.js HTTPS documentation.
-var options = {
+
+
+https.createServer({
   key: fs.readFileSync('privatekey.pem'),
   cert: fs.readFileSync('certificate.pem')
-};
-
-console.log(options);
-
-https.createServer(options, app).listen(3000, ()=> console.log("Rodando express com HTTPS") );
+}, app).listen(3000, ()=> console.log("Rodando express com HTTPS") );
 
 
 
