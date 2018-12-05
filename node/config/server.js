3ec0d581654baf2198cb72443780597d82c59e6f
Cristiano Fagundes <fagundescristianof@gmail.com>
2018-12-05 21:13:49 -0200

    aa

--- node/config/server.js
+++ node/config/server.js
@@ -26,6 +26,12 @@ app.use(expressSession({
 	resave: false,
 	saveUnintialized: false,
 }))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // app.use(cors());
 //app.use(jquery)
 app.use(expressValidator());
