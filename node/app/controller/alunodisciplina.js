d396d5addf7823c1c4ce832ce0e353d6032efca1
Gabriel <gabrielbaldao@gmail.com>
2018-12-09 00:22:44 -0200

    alunodisciplina

--- node/app/controller/alunodisciplina.js
+++ node/app/controller/alunodisciplina.js
@@ -65,20 +65,20 @@ module.exports.get = function(app, req, res){
 module.exports.post = function(app,req,res){
 
   auth.middleware(app,req,res, function(campoToken){
-    console.log("RENATINHA ",req.body)
     auth.verificaAdmin(app,req,res,campoToken, function(campoToken){
 
       var requisicao = req.body;
       var connection = app.config.dbConnection();
       var genericDAO = new app.app.models.GenericDAO(connection);
 
-      genericDAO.create(requisicao,"disciplina", function(error,result){
+      genericDAO.create(requisicao,"alunodisciplina", function(error,result){
         if(error){
           console.log("erro")
           console.log(error);
+          return res.status(400).send({erro: 1});
         }
         else{
-          res.status(200).send(requisicao);
+          return res.status(200).send(requisicao);
         }
       });
 
@@ -94,20 +94,36 @@ module.exports.delete = function(app,req,res){
 
   auth.middleware(app,req,res, function(campoToken){
     auth.verificaAdmin(app,req,res, function(campoToken){
+      var aluno = req.header("aluno");
       var disciplina = req.header("disciplina");
       var connection = app.config.dbConnection();
       var genericDAO = new app.app.models.GenericDAO(connection);
-
-      genericDAO.delete({idDisciplina: disciplina},"disciplina", function(error, result){
+      if(aluno == false){
+      genericDAO.delete({aluno},"alunodisciplina", function(error, result){
         if(error){
           console.log("erro")
           console.log(error);
+          return res.status(400).send({erro: 1});
         }
         else {
-          res.send({deletado: 1})
+        return res.send({deletado: 1})
         }
 
       });
+    }else {
+      genericDAO.delete({disciplina},"alunodisciplina", function(error, result){
+        if(error){
+          console.log("erro")
+          console.log(error);
+          return res.status(400).send({erro: 1});
+        }
+        else {
+        return res.send({deletado: 1})
+        }
+
+      });
+
+    }
 
 
 
@@ -123,15 +139,37 @@ module.exports.put = function(app,req,res){
     var connection = app.config.dbConnection();
     var genericDAO = new app.app.models.GenericDAO(connection);
     console.log("update");
-    genericDAO.update(requisicao, {idDisciplina: requisicao.idDisciplina},"disciplina",function(error, result){
+    let aluno = requisicao.aluno;
+    let disciplina = requisicao.disciplina;
+    if(aluno == false){
+    genericDAO.update(requisicao, {disciplina: req  genericDAO.update(requisicao, {idDisciplina: requisicao.idDisciplina},"disciplina",function(error, result){
+        if(error){
+          console.log("erro")
+          console.log(error);
+          return res.status(400).send({erro: 1});
+        } else {
+        return res.send({atualizado: 1});
+        }
+      });uisicao.disciplina},"disciplina",function(error, result){
       if(error){
         console.log("erro")
         console.log(error);
+        return res.status(400).send({erro: 1});
       } else {
-        res.send({atualizado: 1})
+      return res.send({atualizado: 1});
       }
     });
-
+  }else {
+    genericDAO.update(requisicao, {aluno: requisicao.aluno},"disciplina",function(error, result){
+      if(error){
+        console.log("erro")
+        console.log(error);
+        return res.status(400).send({erro: 1});
+      } else {
+      return res.send({atualizado: 1});
+      }
+    });
+  }
     connection.end();
 
   });
