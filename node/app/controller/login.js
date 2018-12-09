e9cef3883311b03816097a5caa3e9bcd2353105b
Gabriel <gabrielbaldao@gmail.com>
2018-12-09 13:24:23 -0200

    cria aluno e professor

--- node/app/controller/login.js
+++ node/app/controller/login.js
@@ -5,13 +5,16 @@ var auth = require('./auth');
 module.exports.register = function(app, req, res){
   var connection = app.config.dbConnection();
   var genericDAO = new app.app.models.GenericDAO(connection);
-  var {usuario} = req.body;
-  var {senha} = req.body;
+  var {username} = req.body;
+  var {password} = req.body;
   // var usuario = requisicao.usuario;
+  var aluno;
+  var professor;
+
 
   try{
 
-   genericDAO.find({usuario},"usuario",function(error, result){
+   genericDAO.find({usuario: username},"usuario",function(error, result){
     console.log(result.length)
     busca = result.length;
     console.log("busca ", busca)
@@ -19,23 +22,87 @@ module.exports.register = function(app, req, res){
       return res.status(400).send("error: usuario ja existente");
     }
     else {
-      var senhaCriptografada = crypto.createHash('md5').update(senha).digest('hex');
-      usuario = {
-        usuario: usuario,
-        senha: senhaCriptografada,
-        tipo: 0,
-        token: "token ",
-      }
-      genericDAO.create(usuario, "usuario", function(error, result){
+      if(req.body.aluno == 1){ //Cria aluno
+        aluno = {
+          nome: req.body.nome,
+          email: req.body.email,
+          telefone: req.body.telefone,
+          anoinicio: req.body.anoinicio,
+          curso: req.body.curso,
+          semestre: req.body.semestre,
+          bolsista: req.body.bolsista,
 
-        if(error){
-          console.log(error);
-        }
-        res.send({usuario: usuario.usuario, token: token});
+
+        };
+        genericDAO.create(aluno,"aluno", function(err, resultado){
+          if(error){
+            console.log(error);
+            return res.status(400).send("erro ao criar aluno");
+          }
+
+          var senhaCriptografada = crypto.createHash('md5').update(senha).digest('hex');
+          usuario = {
+            usuario: usuario,
+            senha: senhaCriptografada,
+            tipo: 0,
+            aluno: 1,
+            professor: 0,
+            token: "token ",
+          }
+          genericDAO.create(usuario, "usuario", function(error, result){
+
+            if(error){
+              console.log(error);
+              return res.status(400).send("erro ao criar usuario");
+            }
+
+            return res.send({usuario: usuario.usuario, token: token});
+          });
+
       });
     }
-  });
- }  catch(err){
+    if(req.body.professor == 1){ //Cria idProfessor
+      professor = {
+        nome: req.body.professor,
+        email: req.body.email,
+        telefone: req.body.telefone,
+        departamento: req.body.departamento,
+        endereco: req.body.endereco,
+        admin: 0,
+
+      }
+      genericDAO.create(professor,"professor", function(err, resultado){
+        if(error){
+          console.log(error);
+          return res.status(400).send("erro ao criar professor");
+        }
+
+        var senhaCriptografada = crypto.createHash('md5').update(senha).digest('hex');
+        usuario = {
+          usuario: usuario,
+          senha: senhaCriptografada,
+          tipo: 0,
+          aluno: 0,
+          professor: 1,
+          token: "token ",
+        }
+        genericDAO.create(usuario, "usuario", function(error, result){
+
+          if(error){
+            console.log(error);
+            return res.status(400).send("erro ao criar usuario");
+          }
+
+          return res.send({usuario: usuario.usuario, token: token});
+        });
+
+    });
+  }
+      }
+    });
+
+
+} catch(err){
   console.log(err);
 }
 
@@ -43,7 +110,7 @@ module.exports.register = function(app, req, res){
 
 
 }
- function trataUsuario(token, bd){
+function trataUsuario(token, bd){
   if(token.professor > 0){
     return  trataProfessor(token.professor,  bd);
   }
@@ -106,22 +173,22 @@ module.exports.autenticar = function(app, req, res){
 
 
         };
-        	let token2 =  auth.generationToken(trataUsuario(campoToken,genericDAO));
-          let token = auth.generationToken(campoToken);
-          result.Autenticacao = token;
+        let token2 =  auth.generationToken(trataUsuario(campoToken,genericDAO));
+        let token = auth.generationToken(campoToken);
+        result.Autenticacao = token;
 
-          genericDAO.update(result,{id: result.id},"usuario",function(error,result){
-            if(error)
-              {	console.log(error)
-								console.log("erro ao salvar token")}})
-          .then(
-            function(){
-              req.session.autorizado = true;
-              req.session.Autenticacao = token;
-							req.session.Autorizacao = token2;
-              req.session.usuario = usuario;
-              console.log("req session");
-              console.log(req.session);
+        genericDAO.update(result,{id: result.id},"usuario",function(error,result){
+          if(error)
+            {	console.log(error)
+              console.log("erro ao salvar token")}})
+        .then(
+          function(){
+            req.session.autorizado = true;
+            req.session.Autenticacao = token;
+            req.session.Autorizacao = token2;
+            req.session.usuario = usuario;
+            console.log("req session");
+            console.log(req.session);
                     /*
                     res.redirect("")
                     ou nem precisa enviar o token aqui em baixo
@@ -129,7 +196,7 @@ module.exports.autenticar = function(app, req, res){
                     res.send({id: result.id,usuario: usuario, Autenticacao: token, Autorizacao:token2 });
                   }
                   );
-        }
+      }
 
 
       else {
