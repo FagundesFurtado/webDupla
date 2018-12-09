8724c159bd3c2d3ee94a8a9271bbd800ad02fdf4
Gabriel <gabrielbaldao@gmail.com>
2018-12-09 01:03:16 -0200

    aluno get

--- node/app/controller/aluno.js
+++ node/app/controller/aluno.js
@@ -3,7 +3,7 @@ module.exports.get = function(app, req, res){
   var aluno = req.query.aluno;
   var connection = app.config.dbConnection();
   var genericDAO = new app.app.models.GenericDAO(connection);
-  genericDAO.find({idAluno: aluno},"aluno",function(error, result){
+  genericDAO.read("aluno",function(error, result){
     if(error){
       console.log("erro")
       console.log(error);
